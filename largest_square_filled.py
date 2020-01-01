e9a27c7dd8e2335fead503065661b9847c9c7eb3
Alex Lübeck <ndsvw@gmx.de>
2020-01-01 14:16:45 +0100

    space optimization (filled)

--- largest_square_filled.py
+++ largest_square_filled.py
@@ -43,3 +43,51 @@ def calculate_largest_square_filled_first_approach(matrix):
                     ]) + 1
     return max(reduce(lambda e, f: e + f, arr, []))
 
+
+def calculate_largest_square_filled_space_optimized1(matrix):
+    """
+    A method that calculates the largest square filled with only 1's of a given binary matrix (space-optimized 1/2).
+    Problem description: https://practice.geeksforgeeks.org/problems/largest-square-formed-in-a-matrix/0
+        time complexity: O(n*m)
+        space complexity: O(m)
+
+    Parameters
+    ----------
+    matrix : int[[]]
+        a 2-dimensional list
+
+    Returns
+    -------
+    int
+        size of the largest square
+    """
+
+    n = len(matrix)
+    assert n >= 1
+    m = len(matrix[0])
+    assert m >= 1
+
+    arr = [
+        [0 for _ in range(m)],
+        [0 for _ in range(m)]
+    ]
+
+    max_square_size = 0
+
+    for i in range(n):
+        arr[0] = [e for e in arr[1]]
+        for j in range(m):
+            if matrix[i][j] == 0:
+                arr[1][j] = 0
+            else:
+                if i == 0 or j == 0:
+                    arr[1][j] = 1
+                else:
+                    arr[1][j] = min([
+                        arr[0][j],
+                        arr[1][j-1],
+                        arr[0][j-1]
+                    ]) + 1
+                max_square_size = max(arr[1][j], max_square_size)
+    return max_square_size
+
